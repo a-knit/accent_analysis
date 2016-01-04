7c060517deef25512db4e6315d9751345c6002df
Alex Knittel <Alex@Todd-Thiemanns-MacBook-Pro.local>
2016-01-04 10:43:33 -0800

    added work from break including csv files of "u" sound from american and mandarin speakers

--- scrape_data.py
+++ scrape_data.py
@@ -8,18 +8,29 @@ def get_language_urls(index, language):
     urls_to_scrape = (index + 'browse_language.php?function=find&language=' + language)
     return urls_to_scrape
 
-def get_speaker_urls(index, url):
+def get_speaker_urls(index, url, org_filter=None):
+    
+    def det_org(p):
+        meta = str(p).split('</a>')[-1].strip('</p>')
+        meta = meta.strip(',').split()
+        return meta[-1]
+
     speaker_urls = []
     r = requests.get(url)
     soup = BeautifulSoup(r.content)
     content = soup.find(class_='content')
-    for a in content.find_all('a'):
-        speaker_urls.append(index + a['href'])
+    for p in content.find_all('p'):
+        if org_filter:
+            org = det_org(p)
+            if org==org_filter:
+                speaker_urls.append(index + p.find('a')['href'])        
+        else:
+            speaker_urls.append(index + p.find('a')['href'])
     return speaker_urls
 
 def get_audio(index, urls, language):
     audio = []
-    for url in urls:
+    for url in urls[300:]:
         r = requests.get(url)
         soup = BeautifulSoup(r.content)
         content = soup.find(class_='content')
@@ -30,17 +41,19 @@ def get_audio(index, urls, language):
     directory = 'files/' + language + '/'
     if not os.path.exists(directory):
         os.makedirs(directory)
+    os.chdir(directory)
     for mp3 in audio:
         fname = mp3.split('/')[-1]
-        with open(directory + fname, 'wb') as f:
-            print 'Writing file - ', mp3
-            f.write(r)
+        grabThis = 'wget '
+        grabThis = grabThis + mp3
+        os.system(grabThis)
+    os.chdir('/Users/Alex/Documents/gitrepos/accent_analysis')
 
 if __name__ == '__main__':
     index = 'http://accent.gmu.edu/'
-    languages = ['hindi']
+    languages = ['english']
     for language in languages:
         urls_to_scrape = get_language_urls(index, language)
-        speaker_urls = get_speaker_urls(index, urls_to_scrape)
+        speaker_urls = get_speaker_urls(index, urls_to_scrape, 'usa')
         get_audio(index, speaker_urls, language)
     

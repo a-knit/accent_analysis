1a0d3d9a9d61ad13bdb108d52c9652a128646c46
Alex Knittel <Alex@Todd-Thiemanns-MacBook-Pro.local>
2016-01-05 16:44:18 -0800

    scraper updated to scrape metadata into filename and group into directories based on the language family

--- code/scrape_data.py
+++ code/scrape_data.py
@@ -4,56 +4,74 @@ from load_sound_data import load_sound
 import os
 
 
+def lang_assign():
+    euro = ['english', 'spanish', 'french', 'portuguese', 'russian', 'dutch', 'german', 'polish', 'italian', 'macedonian', 'swedish', 'romanian', 'serbian', 'bulgarian']
+    indo = ['persian', 'hindi', 'urdu', 'bengali', 'nepali', 'kurdish', 'punjabi', 'pashto', 'gujarati', 'dari', 'sinhalese']
+    sino = ['cantonese', 'gan', 'hainanese', 'hakka', 'mandarin', 'naxi', 'taiwanese', 'teochew', 'wu', 'xiang', 'burmese', 'tibetan']
+    afro = ['arabic', 'amharic', 'hausa', 'hebrew', 'tigrigna', 'somali']
+    result = {}
+    for lang in euro:
+        result[lang] = 'european'
+    for lang in indo:
+        result[lang] = 'indo_iranian'
+    for lang in sino:
+        result[lang] = 'sino_tibetan'
+    for lang in afro:
+        result[lang] = 'afroasiatic'
+    return result
+
 def get_language_urls(index, language):
     urls_to_scrape = (index + 'browse_language.php?function=find&language=' + language)
     return urls_to_scrape
 
-def get_speaker_urls(index, url, org_filter=None):
+def get_speaker_info(index, url):
     
-    def det_org(p):
+    def get_meta(p):
         meta = str(p).split('</a>')[-1].strip('</p>')
         meta = meta.strip(',').split()
-        return meta[-1]
+        org = meta[-1]
+        gender = meta[0][0]
+        return org, gender
 
-    speaker_urls = []
+    speakers = []
     r = requests.get(url)
     soup = BeautifulSoup(r.content)
     content = soup.find(class_='content')
     for p in content.find_all('p'):
-        if org_filter:
-            org = det_org(p)
-            if org==org_filter:
-                speaker_urls.append(index + p.find('a')['href'])        
-        else:
-            speaker_urls.append(index + p.find('a')['href'])
-    return speaker_urls
+        org, gender = get_meta(p)
+        speakers.append((index + p.find('a')['href'], gender, org))
+    return speakers
 
-def get_audio(index, urls, language):
+def get_audio(index, speakers, language, directory):
     audio = []
-    for url in urls[300:]:
-        r = requests.get(url)
+    for speaker in speakers[125:]:
+        r = requests.get(speaker[0])
         soup = BeautifulSoup(r.content)
         content = soup.find(class_='content')
         source = soup.find('source')['src']
         source = source.split('/')
         source = index + source[-2] + '/' + source[-1]
-        audio.append(source)
-    directory = 'files/' + language + '/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        fname = speaker[1] + '_' + speaker[2] + '_' + source.split('/')[-1]
+        audio.append((source, fname))
+    # os.makedirs(directory)
     os.chdir(directory)
     for mp3 in audio:
-        fname = mp3.split('/')[-1]
-        grabThis = 'wget '
-        grabThis = grabThis + mp3
+        fname = mp3[1]
+        grabThis = 'wget -O ' + fname + ' ' + mp3[0]
         os.system(grabThis)
-    os.chdir('/Users/Alex/Documents/gitrepos/accent_analysis')
 
 if __name__ == '__main__':
+        
+    lang_fam = lang_assign()
+
     index = 'http://accent.gmu.edu/'
-    languages = ['english']
-    for language in languages:
-        urls_to_scrape = get_language_urls(index, language)
-        speaker_urls = get_speaker_urls(index, urls_to_scrape, 'usa')
-        get_audio(index, speaker_urls, language)
     
+    lang = 'spanish'
+    fam = lang_fam[lang]
+
+    directory = '/Users/Alex/accent_sound_files/' + fam + '/' + lang + '/'
+    
+    print 'working on - ', lang
+    urls_to_scrape = get_language_urls(index, lang)
+    speakers = get_speaker_info(index, urls_to_scrape)
+    get_audio(index, speakers, lang, directory)
